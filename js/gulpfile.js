3713eb8f570674dfa20adc8b1ac86600ae762378
larysakutyrkina <kutyrkina.larysa@gmail.com>
2021-03-03 17:33:19 +0200

    ...

---
+++ js/gulpfile.js
@@ -0,0 +1,5 @@
+const gulp = require('gulp');
+const eslint = require('eslint');
+const moment = require('moment');
+const bootstrap = require('bootstrap');
+browserSync = require('browser-sync').create();
